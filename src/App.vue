f0c6cd1220e503c31d54182c32c5ea51f7bc86ae
杨洋 <yangyang@itcast.cn>
2023-03-11 15:51:46 +0800

    state的学习

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <p>user中的states:{{$store.state.user.age}}</p>
+    <p>shopCar中的states:{{$store.state.shopCar.age}}</p>
+    <p>shopCar中的getters:{{$store.getters['user/myAge']}}</p>
+    <button @click="changuserAge">改变user模块中的state</button>
+    <button @click="changshopCarAge">改变shopCar模块的state</button>
+    <router-view />
+  </div>
 </template>
+<script>
+export default {
+  created () {
+    console.log(this)
+  },
+  methods: {
+    changuserAge () {
+      // this.$store.commit('user/changeAge', 30)
+      this.$store.dispatch('user/getAge')
+    },
+    changshopCarAge () {
+      this.$store.commit('shopCar/changeAge', 40)
+    }
+  }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { Store } from 'vuex'
 
 Vue.use(Vuex)
 
@@ -13,5 +13,50 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+
+    user: {
+      // 开启命名空间
+      namespaced: true,
+      state: {
+        age: 10
+      },
+      getters: {
+        myAge (state) {
+          return state.age + 10
+        }
+      },
+      mutations: {
+        changeAge (state, val) {
+          state.age = val
+        }
+      },
+      actions: {
+        getAge (state) {
+          setTimeout(() => {
+            const data = { age: 90 }
+            Store.commit('changeAge', data.age)
+          }, 1000
+          )
+        }
+      }
+    },
+    shopCar: {
+      // 开启命名空间
+      namespaced: true,
+      state: {
+        age: 113
+      },
+      getters: {
+
+      },
+      mutations: {
+        changeAge (state, val) {
+          state.age = val
+        }
+      },
+      actions: {
+
+      }
+    }
   }
 })
